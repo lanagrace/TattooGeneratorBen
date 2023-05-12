1a171c066787684589e06855e1e093789c479f8a
lanagrace <61827763+lanagrace@users.noreply.github.com>
2023-05-12 14:26:46 +0100

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,33 @@
+ var images = []
+ let r =0;
+function preload(){
+ 
+  for(let i=1;i<33;i++){
+   
+
+    append(images, "/Images/img"+i+".jpg");
+/*     console.log(images.length);*/
+  }
+}
+function setup() {
+  noCanvas();
+}
+
+
+function draw() {
+  background(100);
+  
+  image(images[r], 0, 0)
+  
+  }
+
+  function mouseClicked() {
+    r = floor(random(1, images.length));
+
+    var img =  document.getElementById("tattooImage");
+    console.log(img);
+    img.src =images[r];
+
+    }
+  
+  
